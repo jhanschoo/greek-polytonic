b55b73a3cb489311200bf9d5ee471ccc1cd7af54
Johannes Choo <jhanschoo@gmail.com>
2018-07-14 16:57:44 +0800

    Assign copyrighht to FSF

--- greek-polytonic.el
+++ greek-polytonic.el
@@ -1,6 +1,19 @@
 ;;; greek-polytonic.el --- Quail package for inputting polytonic Greek
 
-;; Copyright (C) 2016 Johannes Choo
+;;    Copyright (C) 2018 Free Software Foundation, Inc. <http://fsf.org/>
+
+;;    This program is free software: you can redistribute it and/or modify
+;;    it under the terms of the GNU General Public License as published by
+;;    the Free Software Foundation, either version 3 of the License, or
+;;    (at your option) any later version.
+
+;;    This program is distributed in the hope that it will be useful,
+;;    but WITHOUT ANY WARRANTY; without even the implied warranty of
+;;    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+;;    GNU General Public License for more details.
+
+;;    You should have received a copy of the GNU General Public License
+;;    along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 ;; Author: Johannes Choo <jhanschoo@gmail.com>
 ;; Version: 1.0.3
@@ -10,6 +23,7 @@
 ;; URL: https://github.com/jhanschoo/quail-greek-polytonic
 
 ;;; Commentary:
+
 
 ;; This package provides a method for inputting polytonic Greek, based
 ;; on the Mac and (to a lesser extent) Windows polytonic Greek
